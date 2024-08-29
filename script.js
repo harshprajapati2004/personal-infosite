0a052bb75ad463e39a2bcc91d84edf17c5b57854
harshprajapati2004 <110317541+harshprajapati2004@users.noreply.github.com>
2024-08-29 20:19:11 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,8 @@
-// Toggle Switcher
-const styleSwitcherToggle = document.querySelector(".style-switcher-toggler");
-styleSwitcherToggle.addEventListener("click", () => {
-    document.querySelector(".style-switcher").classList.toggle("open");
+/* ====================TYPINGGGGG ANIMATIONNSS===========================*/
+var typed = new Typed(".typing", {
+    strings: ["Web Designer", "Web Developer", "Professional Trader", "&" ,"Gamer"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true,
 });
 
-// Hide style switcher on scroll
-window.addEventListener("scroll", () => {
-    if (document.querySelector(".style-switcher").classList.contains("open")) {
-        document.querySelector(".style-switcher").classList.remove("open");
-    }
-});
-
-/* ========================== Theme Color ================== */
-const alternateStyles = document.querySelectorAll(".alternate-style");
-function setActiveStyle(color) {
-    alternateStyles.forEach(style => {
-        if (color === style.getAttribute("title")) {
-            style.removeAttribute("disabled");
-        } else {
-            style.setAttribute("disabled", "true");
-        }
-    });
-}
